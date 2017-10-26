19e9eb75cd427203943c68bd94ed72c5fbdce529
Dinesh <dinesh.ls@arkayindia.com>
2017-10-26 16:17:42 +0530

    Initial sharing of project

---
+++ sample_class.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class sample_class here.
+ *
+ * @author (Dinesh Arora)
+ * @version (v001)
+ */
+public class sample_class
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class sample_class
+     */
+    public sample_class()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public static void mian(String[] args) {
+        System.out.println("Hello world!");
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
